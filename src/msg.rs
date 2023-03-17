bfaeeb5836bc257b5a1004eaecab7abe03735a8d
Ben Adams <darwinzero@gmail.com>
2023-03-17 19:51:23 +1300

    initial commit

---
+++ src/msg.rs
@@ -0,0 +1,40 @@
+use cosmwasm_std::{Uint64, Api, StdResult};
+use schemars::JsonSchema;
+use serde::{Deserialize, Serialize};
+
+#[derive(Serialize, Deserialize, Clone, Debug, Eq, PartialEq, JsonSchema)]
+pub struct InstantiateMsg {
+    pub count: i32,
+}
+
+#[derive(Serialize, Deserialize, Clone, Debug, Eq, PartialEq, JsonSchema)]
+#[serde(rename_all = "snake_case")]
+pub enum ExecuteMsg {
+    Increment {},
+    Reset { count: i32 },
+    EvaporateTest { evaporate: Uint64 }
+}
+
+impl ExecuteMsg {
+    pub fn execute_evaporate_gas(&self, api: &dyn Api) -> StdResult<bool> {
+        match self {
+            ExecuteMsg::EvaporateTest { evaporate } => {
+                return api.gas_evaporate(&evaporate.u64());
+            }
+            _ => { Ok(false) }
+        }
+    }
+}
+
+#[derive(Serialize, Deserialize, Clone, Debug, Eq, PartialEq, JsonSchema)]
+#[serde(rename_all = "snake_case")]
+pub enum QueryMsg {
+    // GetCount returns the current count as a json-encoded number
+    GetCount {},
+}
+
+// We define a custom struct for each query response
+#[derive(Serialize, Deserialize, Clone, Debug, Eq, PartialEq, JsonSchema)]
+pub struct CountResponse {
+    pub count: i32,
+}
