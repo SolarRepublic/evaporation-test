7bbf0e831ae020eed76df374e70ac1c4235fa239
Ben Adams <darwinzero@gmail.com>
2024-08-04 09:06:54 +1200

    fix: change param to uint64

--- src/msg.rs
+++ src/msg.rs
@@ -1,4 +1,4 @@
-use cosmwasm_std::{Api, StdResult};
+use cosmwasm_std::{Api, StdResult, Uint64};
 use schemars::JsonSchema;
 use serde::{Deserialize, Serialize};
 
@@ -10,14 +10,14 @@ pub enum InstantiateMsg {
 #[derive(Serialize, Deserialize, Clone, Debug, Eq, PartialEq, JsonSchema)]
 #[serde(rename_all = "snake_case")]
 pub enum ExecuteMsg {
-    EvaporateTest { evaporate: u32 }
+    EvaporateTest { evaporate: Uint64 }
 }
 
 impl ExecuteMsg {
     pub fn execute_evaporate_gas(&self, api: &dyn Api) -> StdResult<()> {
         match self {
             ExecuteMsg::EvaporateTest { evaporate } => {
-                return api.gas_evaporate(*evaporate);
+                api.gas_evaporate(evaporate.u64().try_into().unwrap_or(0))
             }
         }
     }
